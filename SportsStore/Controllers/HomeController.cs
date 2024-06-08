51aef4f6d3b54f98ae507737add44e150b874337
levitimothy <levitimothy@mail.weber.edu>
2024-06-08 00:34:11 -0600

    Finished assignment

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -11,10 +11,12 @@ namespace SportsStore.Controllers
         {
             repository = repo;
         }
-        public ViewResult Index(int productPage = 1)
+        public ViewResult Index(string? category, int productPage = 1)
         => View(new ProductsListViewModel
         {
             Products = repository.Products
+        .Where(p => category == null
+        || p.Category == category)
         .OrderBy(p => p.ProductID)
         .Skip((productPage - 1) * PageSize)
         .Take(PageSize),
@@ -22,8 +24,12 @@ namespace SportsStore.Controllers
             {
                 CurrentPage = productPage,
                 ItemsPerPage = PageSize,
-                TotalItems = repository.Products.Count()
-            }
+                TotalItems = category == null
+        ? repository.Products.Count()
+        : repository.Products.Where(e =>
+        e.Category == category).Count()
+            },
+            CurrentCategory = category
         });
     }
 }
