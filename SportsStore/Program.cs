dbc5969a22345f54a18117ef7e550d9220508c5c
levitimothy <levitimothy@mail.weber.edu>
2024-07-01 22:19:29 -0600

    inital

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SportsStore.Models;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<StoreDbContext>(opts => {
@@ -14,9 +15,17 @@ builder.Services.AddSession();
 builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
 builder.Services.AddSingleton<IHttpContextAccessor,
 HttpContextAccessor>();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+options.UseSqlServer(
+builder.Configuration["ConnectionStrings:IdentityConnection"]));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+.AddEntityFrameworkStores<AppIdentityDbContext>();
 var app = builder.Build();
 app.UseStaticFiles();
 app.UseSession();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllerRoute("catpage",
 "{category}/Page{productPage:int}",
 new { Controller = "Home", action = "Index" });
@@ -29,5 +38,7 @@ app.MapControllerRoute("pagination",
 new { Controller = "Home", action = "Index", productPage = 1 });
 app.MapDefaultControllerRoute();
 app.MapRazorPages();
+app.MapBlazorHub();
+app.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
 SeedData.EnsurePopulated(app);
 app.Run();
